8c38e57953af54a817f65dff080aa3e012c4011c
Anand <110896646+anand22981@users.noreply.github.com>
2025-11-09 21:32:46 +0530

    Layout

--- client/samplebeats/src/component/Layout.jsx
+++ client/samplebeats/src/component/Layout.jsx
@@ -1,14 +1,15 @@
 import { Fragment } from "react";
 import Navbar from "../component/Navbar";
 import Footer from "../component/Footer";
-import Homepage from "../pages/Homepage";
+import Homepage from "../pages/homepage";
+//pages/Homepage.jsx
 
  export default function Layout(){
     return(
         <Fragment>
 
             <Navbar />
-            <Homepage className="pt-16"/>
+            <Homepage/>
             <Footer/>
 
         </Fragment>
