9bda3ba5d345d213a9cd1bacac621282727d58c0
Anand <110896646+anand22981@users.noreply.github.com>
2025-11-09 20:25:20 +0530

    Homepage redesign

---
+++ client/samplebeats/src/Layout/Layout.jsx
@@ -0,0 +1,19 @@
+import { Fragment } from "react";
+import Navbar from "../component/Navbar";
+import Footer from "../component/Footer";
+import Homepage from "../pages/Homepage";
+
+ export default function Layout(){
+    return(
+        <Fragment>
+
+            <Navbar />
+            <Homepage className="pt-16"/>
+            <Footer/>
+
+        </Fragment>
+    )
+}
+
+
+
